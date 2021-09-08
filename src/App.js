41df584bde982fa9c956684841d53f342f115c9e
Admin <nguyenvantuyen6789@gmail.com>
2021-09-08 08:46:27 +0700

    Xong! 2 Cách viết giao diện trong ReactJs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1>This is the first app</h1>
     </div>
-  );
+
+    // Cách 2
+    // React.createElement(
+    //   "div",
+    //   {className: "blue"},
+    //   React.createElement(
+    //     "h1",
+    //     {className: "red"},
+    //     "List Product"
+    //   )
+    // )
+  )
 }
 
 export default App;
