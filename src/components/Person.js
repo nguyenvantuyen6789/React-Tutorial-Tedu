1065fbb709a39d71cbe9356d5fd7c6872583de3e
Admin <nguyenvantuyen6789@gmail.com>
2021-09-08 12:53:36 +0700

    Xong! Liên Kết Với File Css

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "./Person.css"
 
 const Person = (props) => {
+    const styleCSS = {
+        border: "1px solid red",
+        fontWeight: "bold"
+    }
+    
     return <div>
-            <p onClick={props.clickHere}>This is a person {Math.random()}. Name: {props.name}. Age: {props.age}</p>
+            <p style={styleCSS} className="person" onClick={props.clickHere}>This is a person {Math.random()}. Name: {props.name}. Age: {props.age}</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} />
         </div>
