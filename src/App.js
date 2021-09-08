2dbc078122969173c249b71419cd5068528a3d92
Admin <nguyenvantuyen6789@gmail.com>
2021-09-08 10:14:37 +0700

    Xong! Sử Dụng Dữ Liệu Json Trong State Để Passing Data

--- src/App.js
+++ src/App.js
@@ -11,15 +11,34 @@ function App() {
 
   const [count, setCount] = useState(0);
 
+  const [listPerson, setListPerson] = useState([
+    {name: "Tedu 1", age: 5},
+    {name: "Tedu 2", age: 6},
+    {name: "Tedu 3", age: 7}
+  ])
+
+  const setToAnotherPerson = () => {
+    console.log("to another p");
+    setListPerson([
+      {name: "Tedu 4.5", age: 10},
+      {name: "Tedu 6", age: 11},
+      {name: "Tedu 8", age: 12}
+    ])
+  }
+
   return (
     <div className="App">
       <h1>This is the first app</h1>
-      <button onClick={ (e) => changNameHandler(e) }>Change Name</button>
+      <button onClick={(e) => changNameHandler(e)}>Change Name</button>
       <Person name="Tuyên" age="26">Content of Person Tag</Person>
 
       <p>You clicked {count} times</p>
-      <button onClick={ () => setCount(count + 1) }>Tăng Count</button>
+      <button onClick={() => setCount(count + 1)}>Tăng Count</button>
       <Example></Example>
+
+      <h3>Person 1</h3>
+      <Person name={listPerson[0].name} age={listPerson[0].age}></Person>
+      <button onClick={() => setToAnotherPerson()}>Đổi Person</button>
     </div>
 
     // Cách 2
